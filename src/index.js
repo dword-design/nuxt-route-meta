ff79e8fd3dd1f7706e59d519abe1011ec67e27bb
Sebastian Landwehr <info@dword-design.de>
2021-04-05 19:41:12 +0200

    fix: add array and nested object support. also removed additionalProperties config (#21)

--- src/index.js
+++ src/index.js
@@ -1,21 +1,10 @@
 import * as babel from '@babel/core'
 import traverse from '@babel/traverse'
-import {
-  filter,
-  find,
-  forEach,
-  keyBy,
-  mapValues,
-} from '@dword-design/functions'
+import { forEach, omit } from '@dword-design/functions'
+import astToLiteral from 'ast-to-literal'
 import { readFileSync } from 'fs-extra'
 
-export default function (moduleOptions) {
-  const options = {
-    additionalProperties: [],
-    ...this.options.routeMeta,
-    ...moduleOptions,
-  }
-
+export default function () {
   const extractMeta = filename => {
     const vueTemplateCompiler = require('vue-template-compiler')
 
@@ -27,32 +16,15 @@ export default function (moduleOptions) {
       const ast = babel.parseSync(Component.script.content, { filename })
       traverse(ast, {
         ExportDefaultDeclaration: path => {
-          const metaProperty =
-            path.node.declaration.properties
-            |> find(
-              property =>
-                property.type === 'ObjectProperty' &&
-                property.key.name === 'meta'
-            )
-          data =
-            [
-              ...(path.node.declaration.properties
-                |> filter(
-                  property =>
-                    property.type === 'ObjectProperty' &&
-                    property.key.name !== 'meta' &&
-                    options.additionalProperties.includes(property.key.name)
-                )),
-              ...(metaProperty ? metaProperty.value.properties : []),
-            ]
-            |> filter('key.name')
-            |> keyBy('key.name')
-            |> mapValues('value.value')
+          data = path.node.declaration |> astToLiteral
         },
       })
     }
 
-    return data
+    return {
+      ...(data |> omit(['meta'])),
+      ...data.meta,
+    }
   }
   this.extendRoutes(routes =>
     forEach(routes, route => (route.meta = route.component |> extractMeta))
